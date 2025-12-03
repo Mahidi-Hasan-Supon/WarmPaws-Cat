c81105bc0ecc4dfa4d866ffe2c8fbfa8894eb94f
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2025-12-04 00:06:24 +0600

    added

--- src/Compunents/Error.jsx
+++ src/Compunents/Error.jsx
@@ -6,14 +6,14 @@ const Error = () => {
   return (
     <div className="w-7xl mx-auto">
       <Navbar></Navbar>
-      {/* <div className="text-center py-80">
+      <div className="text-center py-80">
         <h1 className="text-4xl font-bold text-red-500">
           404 - Page Not Found
         </h1>
         <p className="mt-4 text-gray-600">
           The page you are looking for does not exist.
         </p>
-      </div> */}
+      </div>
       <Footer></Footer>
     </div>
   );
